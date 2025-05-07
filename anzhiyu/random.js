4171e5e38a001646be5a393723ba891e54c07c24
Smithjoysnd <joker3405177989@126.com>
2025-05-07 16:55:23 +0800

    Site updated: 2025-05-07 16:55:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/05/asd/","2024/10/05/hello-world/","2024/10/15/ad/","2024/12/18/123/","2025/05/06/2025-5-6/","2025/05/06/jk/","2025/05/06/page/"];function toRandomPost(){
+var posts=["2024/10/05/asd/","2024/10/05/hello-world/","2024/10/15/ad/","2024/12/18/123/","2025/05/06/2025-5-6/","2025/05/06/jk/","2025/05/06/page/","2025/05/07/报告/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
